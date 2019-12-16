74f1b7a1c77d85292113e02f28ab994c928fe8a7
Jan Arends <jan.arends@mailbox.org>
2019-12-16 17:22:19 +0100

    added counter with vuex

--- Web-Engineering/10/vueapp.js
+++ Web-Engineering/10/vueapp.js
@@ -6,6 +6,40 @@ new Vue({
     components: { Counter }
 });
 
+const counterStore = new Vuex.Store({
+    state: {
+        count: 0
+    },
+    mutations: {
+        increment(state) {
+            state.count++;
+        },
+        decrement(state) {
+            state.count--;
+        }
+    }
+})
+
+new Vue({
+    el: '#counterwithstore',
+    computed: {
+        counteroutput() {
+            return counterStore.state.count;
+        }
+    },
+    methods: {
+        inc() {
+            counterStore.commit('increment');
+            console.log('Store state: ' + counterStore.state.count);
+        },
+        dec() {
+            counterStore.commit('decrement');
+            console.log('Store state: ' + counterStore.state.count);
+        }
+    }
+});
+
+
 new Vue({
     el: '#vueapp2',
     components: { Navcomp }
