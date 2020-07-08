d07453129446f349000cad6bc9814139ab93860c
Jan Arends <jan.arends@mailbox.org>
2020-07-08 17:36:58 +0200

    wip: refactoring rednerliste

--- Web-Engineering/05/renderliste.js
+++ Web-Engineering/05/renderliste.js
@@ -1,65 +1,64 @@
-// ---- Speaker list -----
 const speakerInput = document.getElementById("speakerinput");
 const addSpeakerBtn = document.getElementById('addspeakerbutton');
 const speakerList = document.getElementById('speakerlist');
 
 const allTimers = [];
 
-addSpeakerBtn.onclick = function () {
-    const inputValue = speakerInput.value;
-    let listItem = document.createElement('li');
-    speakerList.appendChild(listItem);
-    listItem.innerHTML = `${inputValue} <time>00:00:00</time> <button>Stop</button>`;
+function addSecond(li) {
+    const time = li.querySelector('time');
+    let hours = parseInt(time.textContent.substring(0, 2));
+    let minutes = parseInt(time.textContent.substring(3, 5));
+    let seconds = parseInt(time.textContent.substring(6, 8));
 
-    let seconds = 0, minutes = 0, hours = 0;
-
-    addSecond = function () {
-        seconds++;
-        if (seconds >= 60) {
-            seconds = 0;
-            minutes++;
-            if (minutes >= 60) {
+    seconds++;
+    if (seconds >= 60) {
+        seconds = 0;
+        minutes++;
+        if (minutes >= 60) {
+            minutes = 0;
+            hours++;
+            if (hours >= 24) {
+                seconds = 0;
                 minutes = 0;
-                hours++;
-                if (hours >= 24) {
-                    seconds = 0;
-                    minutes = 0;
-                    hours = 0;
-                }
+                hours = 0;
             }
         }
+    }
 
-        let formattedTime = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":"
-            + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":"
-            + (seconds > 9 ? seconds : "0" + seconds);
-        listItem.querySelector('time').innerText = formattedTime;
+    let format = (i) => i > 9 ? i : "0" + i;
+    hours = format(hours);
+    minutes = format(minutes);
+    seconds = format(seconds);
+    time.innerText = `${hours}:${minutes}:${seconds}`;
+};
 
-    };
+addSpeakerBtn.onclick = function () {
+    let listItem = document.createElement('li');
+    speakerList.appendChild(listItem);
+    listItem.innerHTML = `${speakerInput.value} <time>00:00:00</time> <button>Stop</button>`;
 
-    let timerID = setInterval(addSecond, 1000);
+    let timerID = setInterval(addSecond, 1000, listItem);
     allTimers.push(timerID);
 
     const btn = listItem.querySelector('button');
-    let p = [listItem, timerID]
-    btn.onclick = testii.bind(listItem, timerID, addSecond);
+    btn.onclick = toggleTime.bind(listItem, timerID, addSecond);
 
     speakerInput.value = '';
     speakerInput.focus();
 };
 
-function testii(timerID, addSecond) {
-    console.log(this);
-    console.log(addSecond);
-    
+function toggleTime(timerID, addSecond) {
     if (this.lastChild.innerText === 'Stop') {
+        console.log('stopped time', timerID);
+
         clearInterval(timerID);
         console.log('Stopped time.');
         this.lastChild.innerText = 'Start';
         return;
     }
     if (this.lastChild.innerText === 'Start') {
-        timerID = setInterval(addSecond, 1000);
-        console.log('Resumed time.');
+        timerID = setInterval(addSecond, 1000, this);
+        console.log('Resumed time.', timerID);
         this.lastChild.innerText = 'Stop';
     }
 };
