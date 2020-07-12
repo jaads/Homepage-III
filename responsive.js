b46467188d8561864014b90711bca5cbdf8bb6cf
Jan Arends <jan.arends@mailbox.org>
2020-07-12 12:44:21 +0200

    added burger button to collapse sidenav

--- responsive.js
+++ responsive.js
@@ -1,9 +1,24 @@
 const topnav = document.querySelector("#myTopnav");
 
-function myFunction() {
+function openMobileTopNav() {
     if (topnav.className === "topnav") {
         topnav.classList.add("responsive");
     } else {
         topnav.classList.remove("responsive")
-    }    
+    }
+};
+
+const sidenavburger = document.querySelector("#sidenav-burger");
+const sidenav = document.querySelector("#sidenav");
+
+sidenavburger.onclick = function () {
+    sidenav.style.display = "block";
+    sidenavburger.style.display = "none";
+};
+
+const sidenavCloseBtn = document.querySelector('#sidenav-close');
+sidenavCloseBtn.onclick = function () {
+    sidenav.style.display = 'none';
+    sidenavburger.style.display = 'block';
+
 };
